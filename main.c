736a17b45c620428f3e3c0c1669a3f46eb0e37a7
knuos <112151897+knuos@users.noreply.github.com>
2022-11-24 14:53:28 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 #define SIZE 10
 
 int binary_search(int *ary, int key);
-void main(){
+int main(void){
 	int ary[]={1, 3, 4, 7, 9, 12, 16, 17, 20, 23};
 	int key;
 	printf("탐색 값을 입력하시오.\n");
 	scanf("%d", &key);
-	printf("탐색 %d회 성공!\n", binary_search(ary, key));
+	printf("탐색 %d회에 성공!\n", binary_search(ary, key));
 }
 
 int binary_search(int *ary, int key){
