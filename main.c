faa3d040e8482216a418c299a642f0f3e4f8d820
knuos <112151897+knuos@users.noreply.github.com>
2022-11-24 14:19:10 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,67 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TRUE 1
-#define FALSE 0
-#define MAX_VERTICES 50
-#define MAX_STACK_SIZE 50
+#define SIZE 10
 
-typedef struct GraphType {
-	int n;	
-	int adj_mat[MAX_VERTICES][MAX_VERTICES];
-} GraphType;
-int visited[MAX_VERTICES];
-
-void init(GraphType* g)
-{
-	int r, c;
-	g->n = 0;
-	for (r = 0; r<MAX_VERTICES; r++)
-		for (c = 0; c<MAX_VERTICES; c++)
-			g->adj_mat[r][c] = 0;
+int binary_search(int *ary, int key);
+void main(){
+	int ary[]={1, 3, 4, 7, 9, 12, 16, 17, 20, 23};
+	int key;
+	printf("찾고자 하는 값을 입력하시오\n");
+	scanf("%d", &key);
+	printf("탐색 %d회 성공!\n", binary_search(ary, key));
 }
 
-void insert_vertex(GraphType* g, int v)
-{
-	if (((g->n) + 1) > MAX_VERTICES) {
-		fprintf(stderr, "Graph is Full");
-		return;
-	}
-	g->n++;
-}
-
-void insert_edge(GraphType* g, int start, int end)
-{
-	if (start >= g->n || end >= g->n) {
-		fprintf(stderr, "Wrong Vertex Number");
-		return;
-	}
-	g->adj_mat[start][end] = 1;
-	g->adj_mat[end][start] = 1;
-}
-
-void d_search(GraphType* g, int v)
-{
-//아래에 소스 코드를 작성하시오.
-	
-}
-
-int main(void)
-{
-	GraphType *g;
-	g = (GraphType *)malloc(sizeof(GraphType));
-	init(g);
-	for (int i = 0; i<4; i++)
-	insert_vertex(g, i);
-	insert_edge(g, 0, 1);
-	insert_edge(g, 0, 2);
-	insert_edge(g, 0, 3);
-	insert_edge(g, 1, 2);
-	insert_edge(g, 2, 3);
-
-	printf("DFS Search using Stack\n");
-	dfs_search(g, 0);
-	printf("\n");
-	free(g);
-	return 0;
+int binary_search(int *ary, int key){
+//아래에 코드를 작성하시오.	
 }
